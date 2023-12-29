76686658a27e72347c853b5c1d25f35797b98979
james2000s <155133204+james2000s@users.noreply.github.com>
2023-12-28 21:51:37 -0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,64 @@
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
+# Author:   Carlos Rodriguez
+# Project:  FileTypeTransfer
+# Date:     12/25/2023
+# Purpose:  Convert between different file types
 
-"""
-# Welcome to Streamlit!
+import tkinter as tk
+from tkinter import filedialog
+import subprocess
+import re
+import sys
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Forward declarations
+file_path, file_dir = '', ''
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+def convert_to(folder, source, timeout=None):
+    try:
+        args = [libreoffice_exec(), '--headless', '--convert-to', 'pdf', '--outdir', folder, source]
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+        process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout)
+        filename = re.search('-> (.*?) using filter', process.stdout.decode())
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+        if filename is None:
+            raise Exception
+        else:
+            print('else, ' + filename.group(1))
+            return filename.group(1)
+    except Exception as e:
+        print(e)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+def libreoffice_exec():
+    # TODO: Provide support for more platforms
+    if sys.platform == 'win32':
+        return 'C:\\Program Files\\LibreOffice\\program\\swriter.exe'
+    return 'libreoffice'
+
+
+def open_file_dialog():
+    global file_path, file_dir
+
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+
+    file_path = filedialog.askopenfilename()  # Show the file dialog and return the selected file's path
+    file_dir = file_path[:file_path.rfind('/')+1]
+    if file_path:
+        print("Path of Directory: " + file_dir)
+        print("Selected file: ", file_path)
+        # Do something with the selected file path, such as further processing or opening the file
+
+
+def main():
+    global file_path, file_dir
+    # Call the function to open the file dialog
+    open_file_dialog()
+    try:
+        convert_to(file_dir, file_path)
+    except Exception as e:
+        print(e)
+
+
+main()
+
